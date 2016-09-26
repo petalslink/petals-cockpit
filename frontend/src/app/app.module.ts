5961406ddaa9893ecea599547a39e5bc48fdb524
Maxime ROBERT <maxime.robert@linagora.com>
2016-09-26 15:08:10 +0200

    feat: Setup angular-material2 and create basic template
    Icons are loaded but instead of loading them from node_modules folder, it's loaded from google.
    Indeed, there's an error when loading them with sass as import.
    Until it's solved, this is a temporary solution.
    
    This closes #19

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,8 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 // angular-material2 modules
-import { MdButtonModule } from '@angular2-material/button';
-import { MdCardModule } from '@angular2-material/card';
+import {MdCardModule} from '@angular2-material/card';
+import {MdButtonToggleModule} from '@angular2-material/button-toggle';
+import {MdButtonModule} from '@angular2-material/button';
+import {MdSidenavModule} from '@angular2-material/sidenav';
+import {MdToolbarModule} from '@angular2-material/toolbar';
+import {MdIconModule} from '@angular2-material/icon';
+import {MdCheckboxModule} from '@angular2-material/checkbox';
+import {MdRadioModule} from '@angular2-material/radio';
+import {MdInputModule} from '@angular2-material/input';
+import {MdProgressBarModule} from '@angular2-material/progress-bar';
+import {MdTabsModule} from '@angular2-material/tabs';
 
 // ngrx
 import { StoreModule } from '@ngrx/store';
@@ -26,8 +35,17 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule,
     StoreModule.provideStore({ counter: CounterReducer }),
-    MdButtonModule.forRoot(),
-    MdCardModule.forRoot()
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdRadioModule,
+    MdInputModule,
+    MdProgressBarModule,
+    MdTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
