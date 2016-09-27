7c03a8499187c27e9f882e57fa5c1dac01310cd5
Maxime ROBERT <maxime.robert@linagora.com>
2016-09-27 10:26:31 +0200

    feat: Setup angular translate
    There's some problems atm that might occurs :
    - Not working with ChangeDectection.OnPush : https://github.com/ocombe/ng2-translate/issues/243
    - Not working with lazyLoad : https://github.com/ocombe/ng2-translate/issues/232
    - Not working with AOT : https://github.com/ocombe/ng2-translate/issues/218
    
    This closes #28

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from 'ng2-translate/ng2-translate';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('en');
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('en');
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,20 +2,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
+
+// ng2-translate
+import { TranslateModule, TranslateStaticLoader, TranslateLoader } from 'ng2-translate/ng2-translate';
 
 // angular-material2 modules
-import {MdCardModule} from '@angular2-material/card';
-import {MdButtonToggleModule} from '@angular2-material/button-toggle';
-import {MdButtonModule} from '@angular2-material/button';
-import {MdSidenavModule} from '@angular2-material/sidenav';
-import {MdToolbarModule} from '@angular2-material/toolbar';
-import {MdIconModule} from '@angular2-material/icon';
-import {MdCheckboxModule} from '@angular2-material/checkbox';
-import {MdRadioModule} from '@angular2-material/radio';
-import {MdInputModule} from '@angular2-material/input';
-import {MdProgressBarModule} from '@angular2-material/progress-bar';
-import {MdTabsModule} from '@angular2-material/tabs';
+import { MdCardModule } from '@angular2-material/card';
+import { MdButtonToggleModule } from '@angular2-material/button-toggle';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdIconModule } from '@angular2-material/icon';
+import { MdCheckboxModule } from '@angular2-material/checkbox';
+import { MdRadioModule } from '@angular2-material/radio';
+import { MdInputModule } from '@angular2-material/input';
+import { MdProgressBarModule } from '@angular2-material/progress-bar';
+import { MdTabsModule } from '@angular2-material/tabs';
 
 // ngrx
 import { StoreModule } from '@ngrx/store';
@@ -41,6 +44,12 @@ import { appRoutes } from './app.routes';
     BrowserModule,
     FormsModule,
     HttpModule,
+    // translate
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (http: Http) => new TranslateStaticLoader(http, '/assets/i18n', '.json'),
+      deps: [Http]
+    }),
     // routes
     appRoutes,
     // redux store
