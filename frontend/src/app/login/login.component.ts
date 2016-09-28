9ceaf669919f279ceacd922d92c0327b1bb1ffbd
Maxime ROBERT <maxime.robert@linagora.com>
2016-09-28 10:39:57 +0200

    feat: User connection flow
    Use @effects from @ngrx to connect the user.Display the different state when user is loggin in.
    
    Related to #5 and #20 (ngrx)
    
    This closes #21 #22

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { USR_IS_CONNECTING } from '../reducers/user.reducer';
+import { AppState } from '../app.state';
+import { UserState } from '../reducers/user.state';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  private user$: Observable<UserState>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private store: Store<AppState>) {
+    this.user$ = <Observable<UserState>>store.select('user');
   }
 
+  connectUser() {
+    this.store.dispatch({type: USR_IS_CONNECTING});
+  }
 }
