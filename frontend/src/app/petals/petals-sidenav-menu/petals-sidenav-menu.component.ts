516cb22047078e9ce427464503fbd38bc97a32ca
Maxime ROBERT <maxime.robert@linagora.com>
2016-09-30 13:04:51 +0200

    feat: Create search bar in PetalsSidenavMenuComponent
    Search bar will be useful to quickly find buses/containers/components/SU
    
    This closes #56

---
+++ frontend/src/app/petals/petals-sidenav-menu/petals-sidenav-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-petals-sidenav-menu',
+  templateUrl: './petals-sidenav-menu.component.html',
+  styleUrls: ['./petals-sidenav-menu.component.scss']
+})
+export class PetalsSidenavMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- frontend/src/app/petals/petals.module.ts
+++ frontend/src/app/petals/petals.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// angular-material2 modules
+import { MaterialModule } from '@angular/material';
+
+// our components
 import { PetalsComponent } from './petals.component';
+import { PetalsSidenavMenuComponent } from './petals-sidenav-menu/petals-sidenav-menu.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
-  declarations: [PetalsComponent]
+  exports: [
+    PetalsComponent,
+    PetalsSidenavMenuComponent
+  ],
+  declarations: [
+    PetalsComponent,
+    PetalsSidenavMenuComponent
+  ],
+  bootstrap: [PetalsComponent]
 })
 export class PetalsModule { }
