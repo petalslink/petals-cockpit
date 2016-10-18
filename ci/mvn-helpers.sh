510f8c746ece8c492f392cac103736c5e8c2c7df
Victor Noël <victor.noel@linagora.com>
2016-10-18 16:33:23 +0200

    Improve CI subsystem
    - Use our own docker image
    - Always use chromium for tests, no phantomjs
    - Run e2e tests with mocks in the frontend build
    - Lock SNAPSHOT dependencies in backend
    - Ensure the right version of the backend is used for the product

--- ci/mvn-helpers.sh
+++ ci/mvn-helpers.sh
@@ -7,15 +7,6 @@ maven() {
   mvn -s ci/settings.xml --errors -B -U -Dmaven.repo.local=$M2_CACHED_REPOSITORY $@
 }
 
-get_project_version() {
-  maven -q -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec
-}
-
 maven_build() {
   maven -Pjacoco -DaltDeploymentRepository=$M2_STAGING_REPOSITORY $@
-}
-
-import_staging() {
-  local version=$(get_project_version)
-  maven dependency:get -Dartifact=org.ow2.petals:petals-cockpit-server:$version -DremoteRepositories=$M2_STAGING_REPOSITORY
 }
