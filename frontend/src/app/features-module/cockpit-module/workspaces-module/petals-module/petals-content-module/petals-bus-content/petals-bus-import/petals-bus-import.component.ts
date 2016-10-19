13d28049c565f26c37dd28ed85da63de161d67b3
Maxime ROBERT <maxime.robert@linagora.com>
2016-10-19 14:32:06 +0200

    Add a button in petals to import a bus and display a page to enter informations

---
+++ frontend/src/app/features-module/cockpit-module/workspaces-module/petals-module/petals-content-module/petals-bus-content/petals-bus-import/petals-bus-import.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-petals-bus-import',
+  templateUrl: './petals-bus-import.component.html',
+  styleUrls: ['./petals-bus-import.component.scss']
+})
+export class PetalsBusImportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
