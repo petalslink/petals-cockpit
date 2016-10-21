a5a0865156d0059bac449501ea69499f6201b9b0
Victor Noël <victor.noel@linagora.com>
2016-10-21 09:47:37 +0200

    Increase e2e timeout

--- frontend/protractor.conf.js
+++ frontend/protractor.conf.js
@@ -21,7 +21,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 60000,
     print: function() {}
   },
   useAllAngular2AppRoots: true,
