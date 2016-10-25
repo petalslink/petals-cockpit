f34a41c87ce432f995a66b3ced25de3fe645a412
Christophe Chevalier <cchevalier@linagora.com>
2016-10-25 09:40:55 +0200

    To provide access to the configuration of a bus for each components: tabs
    - Update form bus overview
    - Make config a generic bus
    
    Close #100

--- frontend/src/app/features-module/cockpit-module/workspaces-module/petals-module/petals-content-module/petals-bus-content/petals-bus-content.component.ts
+++ frontend/src/app/features-module/cockpit-module/workspaces-module/petals-module/petals-content-module/petals-bus-content/petals-bus-content.component.ts
@@ -2,6 +2,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+// rxjs
+import { Observable } from 'rxjs/Rx';
+
 // ngrx
 import { Store } from '@ngrx/store';
 
@@ -9,7 +12,11 @@ import { Store } from '@ngrx/store';
 import { IBus } from '../../../../../../shared-module/interfaces/petals.interface';
 
 // our states
-import { AppState, AppStateRecord } from '../../../../../../app.state';
+import { AppState } from '../../../../../../app.state';
+import { WorkspacesStateRecord, WorkspacesState } from '../../../../../../shared-module/reducers/workspaces.state';
+
+// our reducers
+import { FETCHING_BUS_CONFIG } from '../../../../../../shared-module/reducers/workspaces.reducer';
 
 @Component({
   selector: 'app-petals-bus-content',
@@ -18,34 +25,42 @@ import { AppState, AppStateRecord } from '../../../../../../app.state';
 })
 export class PetalsBusContentComponent implements OnInit {
   private bus: IBus;
+  private workspaces$: Observable<WorkspacesState>;
+  private workspaces: WorkspacesStateRecord;
+  private idSelectedWorkspace: string;
+  private idBus: string;
 
   // is the bus being imported ?
   private busInImport: boolean;
 
   constructor(private store: Store<AppState>, private route: ActivatedRoute) {
+    this.workspaces$ = <Observable<WorkspacesStateRecord>>this.store.select('workspaces');
   }
 
   ngOnInit() {
-    let store$ = this.store.select('workspaces');
     let params$ = this.route.params;
 
-    store$.combineLatest(params$, (storeRecord: AppStateRecord, params) => {
-      let store = storeRecord.toJS();
+    this.workspaces$.combineLatest(params$, (workspaces: WorkspacesStateRecord, params) => {
+      this.workspaces = workspaces.toJS();
+      this.idSelectedWorkspace = workspaces.selectedWorkspaceId;
+      this.idBus = params.idBus;
 
-      if (typeof store.workspaces === 'undefined' || store.workspaces === null) {
+      if (typeof workspaces.workspaces === 'undefined' || workspaces.workspaces === null) {
         return;
       }
 
-      let workspaceIndex = store.workspaces.findIndex(w => w.id === params['idWorkspace']);
+      let workspaceIndex = workspaces.workspaces.findIndex(w => w.id === params['idWorkspace']);
 
-      this.busInImport = store.workspaces[workspaceIndex].busesInProgress.filter(bus => bus.id === params['idBus']).length > 0;
+      this.busInImport = workspaces.workspaces[workspaceIndex].busesInProgress.filter(bus => bus.id === params['idBus']).length > 0;
 
       if (this.busInImport) {
-        this.bus = store.workspaces[workspaceIndex].busesInProgress.filter(bus => bus.id === params['idBus'])[0];
+        this.bus = workspaces.workspaces[workspaceIndex].busesInProgress.filter(bus => bus.id === params['idBus'])[0];
       }
 
       else {
-        this.bus = store.workspaces[workspaceIndex].buses.filter(bus => bus.id === params['idBus'])[0];
+        this.bus = workspaces.workspaces[workspaceIndex].buses.filter(bus => bus.id === params['idBus'])[0];
+
+        this.store.dispatch({type: FETCHING_BUS_CONFIG, payload: this.idBus});
       }
     })
     .subscribe();
