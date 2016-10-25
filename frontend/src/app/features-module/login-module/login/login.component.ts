077011edd0cae7e8aec6c8443f6c4f63e20d5e8c
Christophe Chevalier <cchevalier@linagora.com>
2016-10-25 10:52:50 +0200

    Autofocus on username field works
    Close #101

--- frontend/src/app/features-module/login-module/login/login.component.ts
+++ frontend/src/app/features-module/login-module/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {Component, ChangeDetectionStrategy, OnInit, ViewChild} from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { USR_IS_CONNECTING } from '../../../shared-module/reducers/user.reducer';
 import { AppState } from '../../../app.state';
 import { UserState } from '../../../shared-module/reducers/user.state';
 import { IUser } from '../../../shared-module/interfaces/user.interface';
+import { MdInput } from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +13,16 @@ import { IUser } from '../../../shared-module/interfaces/user.interface';
   styleUrls: ['login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   private user$: Observable<UserState>;
+  @ViewChild('nameInput') nameInput: MdInput;
 
   constructor(private store: Store<AppState>) {
     this.user$ = <Observable<UserState>>store.select('user');
+  }
+
+  ngOnInit() {
+    this.nameInput.focus();
   }
 
   connectUser(user: IUser) {
