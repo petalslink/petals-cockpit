34b6f3f4b2efaec2ef009b4fcfd33b3494432303
Victor Noël <victor.noel@linagora.com>
2016-11-03 16:35:04 +0100

    feat(backend): retrieve bus info from Petals container
    - Exploits petals-admin-api to retrieve the topology information
      from an actual container
    - Nothing is stored in the database for now.

--- backend/src/main/java/org/ow2/petals/cockpit/server/actors/WorkspaceActor.java
+++ backend/src/main/java/org/ow2/petals/cockpit/server/actors/WorkspaceActor.java
@@ -16,10 +16,10 @@
  */
 package org.ow2.petals.cockpit.server.actors;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.ws.rs.core.MediaType;
 
@@ -30,22 +30,34 @@ import org.glassfish.jersey.media.sse.OutboundEvent;
 import org.glassfish.jersey.media.sse.SseBroadcaster;
 import org.glassfish.jersey.server.BroadcasterListener;
 import org.glassfish.jersey.server.ChunkedOutput;
+import org.ow2.petals.admin.api.ContainerAdministration;
+import org.ow2.petals.admin.api.PetalsAdministration;
+import org.ow2.petals.admin.api.PetalsAdministrationFactory;
+import org.ow2.petals.admin.api.artifact.Component;
+import org.ow2.petals.admin.api.artifact.ServiceAssembly;
+import org.ow2.petals.admin.api.artifact.ServiceUnit;
+import org.ow2.petals.admin.api.exception.ContainerAdministrationException;
+import org.ow2.petals.admin.topology.Container;
+import org.ow2.petals.admin.topology.Domain;
 import org.ow2.petals.cockpit.server.actors.WorkspaceActor.Msg;
 import org.ow2.petals.cockpit.server.resources.BusesResource.NewBus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.allanbank.mongodb.bson.Document;
+import com.allanbank.mongodb.bson.builder.ArrayBuilder;
 import com.allanbank.mongodb.bson.builder.BuilderFactory;
-import com.allanbank.mongodb.bson.json.Json;
+import com.allanbank.mongodb.bson.builder.DocumentBuilder;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import co.paralleluniverse.actors.ActorRegistry;
 import co.paralleluniverse.actors.BasicActor;
+import co.paralleluniverse.common.util.CheckedCallable;
 import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.FiberAsync;
 import co.paralleluniverse.fibers.SuspendExecution;
 import co.paralleluniverse.fibers.Suspendable;
 import co.paralleluniverse.strands.Strand;
-import co.paralleluniverse.strands.SuspendableRunnable;
 
 /**
  * TODO should we make it die after some time if there is no listeners?
@@ -61,6 +73,12 @@ public class WorkspaceActor extends BasicActor<Msg, Void> {
 
     @Nullable
     private static ServiceLocator serviceLocator;
+
+    /**
+     * This needs to have only ONE thread because petals-admin uses a singleton which prevent concurrent use
+     */
+    private static final ExecutorService exec = Executors.newFixedThreadPool(1,
+            new ThreadFactoryBuilder().setNameFormat("petals-admin-worker-%d").setDaemon(true).build());
 
     private final String id;
 
@@ -113,8 +131,21 @@ public class WorkspaceActor extends BasicActor<Msg, Void> {
                 // this one is coming from the SSE resource
                 broadcaster.add(((NewClient) msg).output);
             } else if (msg instanceof ImportBus) {
+                ImportBus bus = (ImportBus) msg;
                 // we use a fiber to let the actor handles other message during bus import
-                new Fiber<>(new DoImportBus((ImportBus) msg)).start();
+                new Fiber<>(() -> {
+                    Document d;
+                    try {
+                        Document nd = FiberAsync.runBlocking(exec,
+                                (CheckedCallable<Document, Exception>) () -> doImportBus(bus));
+                        d = BuilderFactory.start().add("event", "BUS_IMPORT_OK").add("data", nd).build();
+                    } catch (Exception e) {
+                        LOG.info("Can't retrieve topology from container {}:{}", bus.nb.getIp(), bus.nb.getPort(), e);
+                        d = BuilderFactory.start().add("event", "BUS_IMPORT_ERROR").add("data", e.getMessage()).build();
+                    }
+                    // we use send in order to keep concurrency under control by the actor!
+                    self().send(new SseEvent("WORKSPACE_CHANGE", d));
+                }).start();
             } else if (msg instanceof SseEvent) {
                 LOG.debug("Sending SSE event to clients for workspace {}: {}", id, msg);
                 broadcaster.broadcast(((SseEvent) msg).event);
@@ -124,39 +155,68 @@ public class WorkspaceActor extends BasicActor<Msg, Void> {
         }
     }
 
-    public class DoImportBus implements SuspendableRunnable {
+    private static Document doImportBus(ImportBus bus) throws Exception {
+        PetalsAdministration petals = PetalsAdministrationFactory.getInstance().newPetalsAdministrationAPI();
+        ContainerAdministration container = petals.newContainerAdministration();
 
-        private static final long serialVersionUID = 3013726321869644100L;
-
-        private final ImportBus bus;
-
-        public DoImportBus(ImportBus bus) {
-            this.bus = bus;
-        }
-
-        @Override
-        public void run() throws SuspendExecution, InterruptedException {
-
-            Fiber.sleep(2000);
-
-            InputStream json = getClass().getResourceAsStream("/demo/" + bus.nb.getIp() + ".json");
-
-            if (json != null) {
-                try (Reader isr = new InputStreamReader(json)) {
-                    Document nd = BuilderFactory.start(Json.parse(isr)).add("id", bus.id).add("name", bus.nb.getIp())
-                            .build();
-                    Document d = BuilderFactory.start().add("event", "BUS_IMPORT_OK").add("data", nd).build();
-                    // we use send in order to keep concurrency under control by the actor!
-                    self().send(new SseEvent("WORKSPACE_CHANGE", d));
-                } catch (IOException e) {
-                    LOG.warn("Exception while closing reader", e);
-                }
-            } else {
-                Document d = BuilderFactory.start().add("event", "BUS_IMPORT_ERROR")
-                        .add("data", "can't connect to " + bus.nb.getIp()).build();
-                self().send(new SseEvent("WORKSPACE_CHANGE", d));
+        try {
+            container.connect(bus.nb.getIp(), bus.nb.getPort(), bus.nb.getUsername(), bus.nb.getPassword());
+            Domain topology = container.getTopology(".*", null, true);
+            return buildBusTree(bus.id, topology);
+        } finally {
+            try {
+                container.disconnect();
+            } catch (ContainerAdministrationException e) {
+                LOG.warn("Error while disconnecting from container", e);
             }
         }
+    }
+
+    private static Document buildBusTree(String id, Domain topology) {
+        DocumentBuilder b = BuilderFactory.start();
+
+        b.add("id", id);
+        b.add("name", topology.getName());
+
+        ArrayBuilder cs = b.pushArray("containers");
+
+        for (Container container : topology.getContainers()) {
+            DocumentBuilder cont = cs.push();
+            // TODO id
+            cont.add("id", "a");
+            cont.add("name", container.getContainerName());
+            cont.add("state", "Deployed");
+            ArrayBuilder comps = cont.pushArray("components");
+            Map<String, @Nullable ArrayBuilder> compsMap = new HashMap<>();
+            for (Component component : container.getComponents()) {
+                DocumentBuilder comp = comps.push();
+                // TODO id
+                comp.add("id", "a");
+                comp.add("name", component.getName());
+                comp.add("state", component.getState().toString());
+                compsMap.put(component.getName(), comp.pushArray("serviceUnits"));
+            }
+
+            for (ServiceAssembly sa : container.getServiceAssemblies()) {
+                for (ServiceUnit su : sa.getServiceUnits()) {
+                    String tc = su.getTargetComponent();
+                    ArrayBuilder sus = compsMap.get(tc);
+                    if (sus != null) {
+                        DocumentBuilder sub = sus.push();
+                        // TODO id
+                        sub.add("id", "a");
+                        sub.add("name", su.getName());
+                        // TODO bof bof
+                        sub.add("state", sa.getState().toString());
+                    } else {
+                        LOG.warn("Retrieved a SU {} deployed on component {}, but {} does not exist", su.getName(), tc,
+                                tc);
+                    }
+                }
+            }
+        }
+
+        return b.build();
     }
 
     public interface Msg {
