7534c32f3cbdb2b46e8e6a01d8ea829bee25ffd1
Maxime ROBERT <maxime.robert@linagora.com>
2016-11-16 16:14:49 +0100

    deploy generated folder with mock on gitlab

---
+++ frontend/src/environments/environment.gitlab-pages.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  mock: true,
+  urlBackend: '/api',
+  debug: true,
+  // auto log user or not
+  // useful for dev env to avoid being redirected to /login every time the page is reloaded
+  alreadyConnected: false,
+  // delay for each http request (used if mock is set to true)
+  httpDelay: 500,
+  // delay for sse events (between 15 and 30s)
+  get sseDelay(): number {
+    let rand = (Math.floor(Math.random() * (1 + 30 - 15)) + 15) * 1000;
+    console.debug(`sse random timer : ${rand}`);
+    return rand;
+  },
+  sseFirstBusImportShouldFail: true
+};
