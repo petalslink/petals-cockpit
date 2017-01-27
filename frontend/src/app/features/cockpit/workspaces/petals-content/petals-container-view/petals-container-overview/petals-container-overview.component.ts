230c58ba6882ebdf5dcf86e317418472daad8e42
Maxime ROBERT <maxime.robert@linagora.com>
2017-01-27 08:46:20 +0100

    prepare the link for reachabilities on container view

--- frontend/src/app/features/cockpit/workspaces/petals-content/petals-container-view/petals-container-overview/petals-container-overview.component.ts
+++ frontend/src/app/features/cockpit/workspaces/petals-content/petals-container-view/petals-container-overview/petals-container-overview.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
 
 import { IContainerRow } from '../../../state/containers/container.interface';
+import { IStore } from './../../../../../../shared/interfaces/store.interface';
 
 @Component({
   selector: 'app-petals-container-overview',
@@ -9,9 +12,13 @@ import { IContainerRow } from '../../../state/containers/container.interface';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PetalsContainerOverviewComponent implements OnInit {
+  public idWorkspace$: Observable<string>;
+
   @Input() container: IContainerRow;
 
-  constructor() { }
+  constructor(private _store$: Store<IStore>) {
+    this.idWorkspace$ = this._store$.select(state => state.workspaces.selectedWorkspaceId);
+  }
 
   ngOnInit() {
   }
