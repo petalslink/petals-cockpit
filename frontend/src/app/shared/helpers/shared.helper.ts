5de317ac767ba467d29954671eb67074d4f861ef
Maxime ROBERT <maxime.robert@linagora.com>
2017-01-30 09:44:58 +0100

    fix helper to add id property on object

--- frontend/src/app/shared/helpers/shared.helper.ts
+++ frontend/src/app/shared/helpers/shared.helper.ts
@@ -1,19 +1,19 @@
 // generate a UUID
 export function generateUuidV4(a = null) {
   /* tslint:disable */
-  return a?(a^Math.random()*16>>a/4)
-    .toString(16):(<any>[1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,generateUuidV4);
+  return a ? (a ^ Math.random() * 16 >> a / 4)
+    .toString(16) : (<any>[1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, generateUuidV4);
   /* tslint:enable */
 }
 
-// careful this modify the object in-place
 export function toJavascriptMap(map: any) {
   const allIds = Object.keys(map);
-  allIds.forEach(id => {
-    map[id].id = id;
-  });
-  return {
-    byId: map,
-    allIds: allIds
-  };
+
+  const byId = {};
+
+  allIds.forEach(id =>
+    byId[id] = Object.assign({}, map[id], { id })
+  );
+
+  return { byId, allIds };
 }
