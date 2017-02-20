7b0b61c61f39f738e41b572b5091b46add747374
Victor Noël <victor.noel@linagora.com>
2017-02-20 16:20:25 +0100

    Remove unused code [ci skip]

--- frontend/src/app/features/cockpit/cockpit.module.ts
+++ frontend/src/app/features/cockpit/cockpit.module.ts
@@ -21,7 +21,6 @@ import { SharedModule } from '../../shared/shared.module';
 import { CockpitRoutingModule } from './cockpit-routing.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { CockpitComponent } from './cockpit.component';
-import { EmptyMenuComponent } from './empty-menu/empty-menu.component';
 import { WorkspacesDialogComponent } from './workspaces-dialog/workspaces-dialog.component';
 import { MenuUserPanelComponent } from './menu-user-panel/menu-user-panel.component';
 
@@ -33,7 +32,6 @@ import { MenuUserPanelComponent } from './menu-user-panel/menu-user-panel.compon
   ],
   declarations: [
     CockpitComponent,
-    EmptyMenuComponent,
     WorkspacesDialogComponent,
     MenuUserPanelComponent
   ],
