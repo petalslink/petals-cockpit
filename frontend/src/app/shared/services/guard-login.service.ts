8e5c6b03b15545920d2d1400c7ff16f4dca170d7
Victor Noël <victor.noel@linagora.com>
2017-02-20 15:31:22 +0100

    Remove duplicate copyright header [ci skip]

--- frontend/src/app/shared/services/guard-login.service.ts
+++ frontend/src/app/shared/services/guard-login.service.ts
@@ -15,24 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { IUser } from './../interfaces/user.interface';
-/**
- * Copyright (C) 2017 Linagora
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
 import { Response } from '@angular/http';
@@ -43,6 +25,7 @@ import { UsersService } from './users.service';
 import { environment } from './../../../environments/environment';
 import { IStore } from './../interfaces/store.interface';
 import { Users } from './../state/users.reducer';
+import { IUser } from './../interfaces/user.interface';
 
 @Injectable()
 export class GuardLoginService implements CanLoad {
