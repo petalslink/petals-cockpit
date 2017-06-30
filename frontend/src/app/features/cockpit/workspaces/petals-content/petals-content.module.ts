d306cfc48f676436c874fcb8631eda85175671ce
Victor Noël <victor.noel@linagora.com>
2017-06-30 14:07:44 +0200

    frontend: remove useless component

--- frontend/src/app/features/cockpit/workspaces/petals-content/petals-content.module.ts
+++ frontend/src/app/features/cockpit/workspaces/petals-content/petals-content.module.ts
@@ -17,12 +17,10 @@
 
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from '../../../../shared/shared.module';
+import { SharedModule } from 'app/shared/shared.module';
 import { PetalsContentRoutingModule } from './petals-content-routing.module';
-import { PetalsContentViewComponent } from './petals-content.component';
 
 @NgModule({
   imports: [SharedModule, PetalsContentRoutingModule],
-  declarations: [PetalsContentViewComponent],
 })
 export class PetalsContentModule {}
