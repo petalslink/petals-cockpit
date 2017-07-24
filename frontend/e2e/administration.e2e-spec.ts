adf632086d7fc1c5fcdade390430d22e3897ba1b
Christophe Chevalier <cchevalier@linagora.com>
2017-07-24 12:06:22 +0200

    fix bug display of expansion panel in mobile mode
    - improve animation mode of accordion with flat mode
    - clean code
    
    Closes #376

--- frontend/e2e/administration.e2e-spec.ts
+++ frontend/e2e/administration.e2e-spec.ts
@@ -171,6 +171,21 @@ describe(`Administration`, () => {
       editVnoel.nameInput.sendKeys(`Victor NOEL`);
       editVnoel.saveButton.click();
     });
+
+    it('should not show a user-name in panel list users', () => {
+      browser.manage().window().setSize(412, 732);
+
+      expect(
+        getMultipleElementsTexts(admin.panelListUsers, '.user-id')
+      ).toEqual([
+        ['admin'],
+        ['bescudie'],
+        ['cchevalier'],
+        ['cdeneux'],
+        ['mrobert'],
+        ['vnoel'],
+      ]);
+    });
   });
 
   it('should not be visible to non-admin', () => {
