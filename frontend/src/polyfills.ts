26a1514084faff239605c81352a4758656410f51
Maxime ROBERT <maxime.robert@linagora.com>
2017-07-28 15:58:12 +0200

    chore: tslint rule to order imports

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -14,6 +14,7 @@
  * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
 
+ // tslint:disable:ordered-imports
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
