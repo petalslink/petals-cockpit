b40b9a147b4d9426a2a4b9e4d20304d355de8acd
Maxime ROBERT <maxime.robert@linagora.com>
2017-08-28 14:16:30 +0200

    feat: add a deletable operator to keep a value and display it one last time before removing it for good

---
+++ frontend/src/app/shared/operators/deletable.operator.ts
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2017 Linagora
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Observable } from 'rxjs/Observable';
+
+export interface IDeletable<T> {
+  isDeleted: boolean;
+  value: T;
+}
+
+export function deletable<T>(obs: Observable<T>): Observable<IDeletable<T>> {
+  return obs
+    .scan((acc: IDeletable<T>, curr: T) => {
+      if (acc !== null && (curr === null || curr === undefined)) {
+        return {
+          isDeleted: true,
+          value: acc.value,
+        };
+      } else {
+        return {
+          isDeleted: false,
+          value: curr,
+        };
+      }
+    }, null)
+    .distinctUntilChanged(
+      (p, n) => p.isDeleted === n.isDeleted && p.value === n.value
+    );
+}
