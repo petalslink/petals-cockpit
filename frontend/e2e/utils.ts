6817ecf9e155e04c6074dd5eb8d2f020d7f84858
Victor Noël <victor.noel@linagora.com>
2017-10-03 16:50:42 +0200

    chore(frontend): blacklist lodash also

--- frontend/e2e/utils.ts
+++ frontend/e2e/utils.ts
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// tslint:disable:import-blacklist because lodash-es imports doesn't work with e2e tsconfig
 import { range } from 'lodash';
 import {
   $,
