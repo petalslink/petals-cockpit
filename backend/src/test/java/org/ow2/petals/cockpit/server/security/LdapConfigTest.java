b110ad42725c341f49c68c9d13d07ba4962862d5
Alexandre Lagane <alagane@linagora.com>
2018-07-23 15:35:43 +0200

    refactor(backend): rename ldap config tests

--- backend/src/test/java/org/ow2/petals/cockpit/server/security/LdapConfigTest.java
+++ backend/src/test/java/org/ow2/petals/cockpit/server/security/LdapConfigTest.java
@@ -51,33 +51,33 @@ public class LdapConfigTest {
      * @throws Exception
      */
     @Test
-    public void testBuildEmptyLdapConfig() throws Exception {
+    public void buildEmptyLdapConfig() throws Exception {
         final File yml = new File(Resources.getResource("ldap-no-config-empty-ldap.yml").toURI());
         CockpitConfiguration config = this.factory.build(yml);
         assertNull(config.getLdapConfigFactory());
     }
 
     @Test
-    public void testBuildOmittedLdapConfig() throws Exception {
+    public void buildOmittedLdapConfig() throws Exception {
         final File yml = new File(Resources.getResource("ldap-no-config-omitted-ldap.yml").toURI());
         CockpitConfiguration config = this.factory.build(yml);
         assertNull(config.getLdapConfigFactory());
     }
 
     @Test(expected = ConfigurationValidationException.class)
-    public void testBuildWrongLdapConfigEmptyField() throws Exception {
+    public void buildWrongLdapConfigEmptyField() throws Exception {
         final File yml = new File(Resources.getResource("ldap-wrong-config-empty-field.yml").toURI());
         this.factory.build(yml);
     }
 
     @Test(expected = ConfigurationValidationException.class)
-    public void testBuildWrongLdapConfigOmittedField() throws Exception {
+    public void buildWrongLdapConfigOmittedField() throws Exception {
         final File yml = new File(Resources.getResource("ldap-wrong-config-omitted-field.yml").toURI());
         this.factory.build(yml);
     }
 
     @Test
-    public void testBuildRightLdapConfig() throws Exception {
+    public void buildRightLdapConfig() throws Exception {
         final File yml = new File(Resources.getResource("ldap-right-config.yml").toURI());
         LdapConfigFactory ldapConf = this.factory.build(yml).getLdapConfigFactory();
 
