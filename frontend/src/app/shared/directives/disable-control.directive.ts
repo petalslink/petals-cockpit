56e95bd132171e9a4dd415c87cca67b5f7aa8aa6
christophechevalier <mr.christophe.chevalier@gmail.com>
2020-07-09 17:41:18 +0200

    refactor(frontend): restricted action adminWorkspace perm
    - create new selector to get needed permission
    - clean code

--- frontend/src/app/shared/directives/disable-control.directive.ts
+++ frontend/src/app/shared/directives/disable-control.directive.ts
@@ -15,17 +15,22 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Directive, Input } from '@angular/core';
+import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appDisableControl]',
 })
-export class DisableControlDirective {
-  @Input()
-  set appDisableControl(isDisabled: boolean) {
-    const action = isDisabled ? 'disable' : 'enable';
-    this.ngControl.control[action]();
+export class DisableControlDirective implements OnChanges {
+  /**
+   * ngOnChanges seems avoid this error: Cannot read property 'disable' of undefined.
+   */
+  @Input() appDisableControl: NgControl;
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['appDisableControl']) {
+      const action = this.appDisableControl ? 'disable' : 'enable';
+      this.ngControl.control[action]();
+    }
   }
 
   constructor(private ngControl: NgControl) {}
