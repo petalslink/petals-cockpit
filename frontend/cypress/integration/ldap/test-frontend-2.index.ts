0df089546c988898655896eeb00a553c2d22e269
christophechevalier <mr.christophe.chevalier@gmail.com>
2020-07-23 17:36:52 +0200

    chore(frontend): add multiple index of spec referencing

---
+++ frontend/cypress/integration/ldap/test-frontend-2.index.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2018-2020 Linagora
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Index of spec referencing all tests in ldap mode
+// tslint:disable:ordered-imports
+import './test-frontend-2/workspace.e2e-spec';
+import './test-frontend-2/workspace-overview-buses.e2e-spec';
+import './test-frontend-2/workspace-overview-users.e2e-spec';
+import './test-frontend-2/petals.e2e-spec';
+import './test-frontend-2/buses.e2e-spec';
+import './test-frontend-2/container.e2e-spec';
+import './test-frontend-2/component.e2e-spec';
+import './test-frontend-2/service-assembly.e2e-spec';
+import './test-frontend-2/shared-library.e2e-spec';
+import './test-frontend-2/service-unit.e2e-spec';
